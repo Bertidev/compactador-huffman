59a9e698e1dcdff245c9582ed072d64226dcef5a
Lucas Berti <111362675+Bertidev@users.noreply.github.com>
2026-04-29 10:10:17 -0300

    Update huffman.h

--- huffman.h
+++ huffman.h
@@ -3,10 +3,10 @@
 #include "meustipos.h"
 #include "codigo.h"
 
-typedef struct No {
-    U8 caracter;
-    U32 frequencia;
-    struct No *esquerda, *direita;
+typedef struct No {        //arvore binaria que sera usada
+    U8 caracter;           //um byte que representa um caracter
+    U32 frequencia;        //quantas vezes o caracter aparece 
+    struct No *esquerda, *direita;    //ponteiros para os filhos da arvore
 } No;
 
 void compactar(const char* arquivo_entrada, const char* arquivo_saida);
